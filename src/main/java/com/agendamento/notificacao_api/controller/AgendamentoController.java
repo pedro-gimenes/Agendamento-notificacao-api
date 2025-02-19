82137167a810d118fb565b17ced68b6fe7002a4f
pedro-gimenes <pdrgimenes@gmail.com]>
2025-02-19 14:14:56 -0300

    adicionados metodfos de cancelamento de notificações

--- src/main/java/com/agendamento/notificacao_api/controller/AgendamentoController.java
+++ src/main/java/com/agendamento/notificacao_api/controller/AgendamentoController.java
@@ -19,8 +19,15 @@ public class AgendamentoController {
     }
 
     @GetMapping("/id")
-    public ResponseEntity<AgendamentoRecordOut> buscarAgendamentoPorId(@PathVariable("id")Long id){
+    public ResponseEntity<AgendamentoRecordOut> buscarAgendamentoPorId(@PathVariable("id") Long id){
         return ResponseEntity.ok(agendamentoService.buscarAgendamentosPorId(id));
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> cancelarAgendamento(@PathVariable("id") Long id) {
+        agendamentoService.cancelarAgendamento(id);
+        return ResponseEntity.accepted().build();
+    }
+
 
 }
